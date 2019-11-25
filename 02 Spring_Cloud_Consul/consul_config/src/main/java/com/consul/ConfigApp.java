8c2624c5d989967c4db95d9ed8ba5898b9609fd1
wangmh <wang732276349@163.com>
2019-11-25 18:13:07 +0800

    更新

--- 02 Spring_Cloud_Consul/consul_config/src/main/java/com/consul/ConfigApp.java
+++ 02 Spring_Cloud_Consul/consul_config/src/main/java/com/consul/ConfigApp.java
@@ -1,8 +1,11 @@
 package com.consul;
 
+import com.consul.config.StudentConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -20,17 +23,27 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @EnableScheduling//必须开启 定时任务
 @RefreshScope
+@EnableConfigurationProperties({StudentConfig.class})
 public class ConfigApp {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApp.class, args);
     }
 
+    //consul中配置目录规则：spring.cloud.consul.config.prefix/spring.cloud.consul.config.name/spring.cloud.consul.config.data-key
     @Value("${foo.bar.name}")
     private String name;
 
     @GetMapping("/getName")
-    public String getName(){
+    public String getName() {
         return name;
+    }
+
+    @Autowired
+    private StudentConfig studentConfig;
+
+    @GetMapping("/getStu")
+    public String getStu() {
+        return studentConfig.toString();
     }
 
     //url和配置文件中的spring.cloud.consul.discovery.health-check-path值相同
---
+++ 02 Spring_Cloud_Consul/consul_config/src/main/java/com/consul/config/StudentConfig.java
@@ -0,0 +1,45 @@
+package com.consul.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "stu")
+public class StudentConfig {
+    private String name;
+    private int age;
+    private String sex;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentConfig{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+}
